9181ff66d96bd62bd8a38d0a9951034020c5398a
Santiago Forero <santiago.forero-y@mail.escuelaing.edu.co>
2023-09-25 16:36:56 -0500

    Avance parte 1

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -8,17 +8,35 @@ package edu.eci.arsw.blueprints.controllers;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
 
 /**
  *
  * @author hcadavid
  */
+@RestController
+@RequestMapping(value="/blueprints")
 public class BlueprintAPIController {
     
-    
+    @Autowired
+    BlueprintsServices bps;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> getAllBlueprints(){
+        try {
+            return new ResponseEntity<Set<Blueprint>>(this.bps.getAllBlueprints(), HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            // TODO: handle exception
+            return new ResponseEntity<String>("Error trying to find blueprints", HttpStatus.NOT_FOUND);
+        }
+    }
     
     
     
---
+++ src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.blueprints.persistence.impl;
+
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.model.Point;
+import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
+import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
+import edu.eci.arsw.blueprints.persistence.BlueprintsPersistence;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author hcadavid
+ */
+ @Service
+public class InMemoryBlueprintPersistence implements BlueprintsPersistence {
+
+    private final Map<Tuple<String, String>, Blueprint> blueprints = new HashMap<>();
+
+    public InMemoryBlueprintPersistence() {
+       
+        Blueprint bp1 = new Blueprint("john", "thepaint", new Point[] { new Point(0, 0), new Point(10, 10) });
+
+        Blueprint bp2 = new Blueprint("Martha", "thepaint1", new Point[] { new Point(10, 15), new Point(20, 20) });
+
+        Blueprint bp3 = new Blueprint("john", "thepaint2", new Point[] { new Point(16, 7), new Point(30, 30) });
+
+        try {
+            this.saveBlueprint(bp1);
+            this.saveBlueprint(bp2);
+            this.saveBlueprint(bp3);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    @Override
+    public Set<Blueprint> getAllBlueprints() throws BlueprintNotFoundException {
+        Set<Blueprint> resp = new HashSet<>();
+
+        for (Map.Entry<Tuple<String, String>, Blueprint> space : blueprints.entrySet()) {
+            resp.add(space.getValue());
+        }
+        return resp;
+    }
+
+    @Override
+    public void saveBlueprint(Blueprint bp) throws BlueprintPersistenceException {
+        if (blueprints.containsKey(new Tuple<>(bp.getAuthor(), bp.getName()))) {
+            throw new BlueprintPersistenceException("The given blueprint already exists: " + bp);
+        } else {
+            blueprints.put(new Tuple<>(bp.getAuthor(), bp.getName()), bp);
+        }
+    }
+
+    @Override
+    public Blueprint getBlueprint(String author, String bprintname) throws BlueprintNotFoundException {
+        return blueprints.get(new Tuple<>(author, bprintname));
+    }
+
+    @Override
+    public Set<Blueprint> getBlueprintsByAuthor(String author) throws BlueprintNotFoundException {
+        Set<Blueprint> resp = new HashSet<>();
+
+        for (Map.Entry<Tuple<String, String>, Blueprint> space : blueprints.entrySet()) {
+            if (space.getKey().getElem1().equals(author)) {
+                resp.add(space.getValue());
+            }
+        }
+        return resp;
+
+    }
+}
